5d6a433fec9550f8ba79e10f184948bb54a9d22e
Abraham F. <39970401+hackit92@users.noreply.github.com>
2025-07-02 16:44:05 -0600

    Fix SVG import error by adding vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   base: "./",
   css: {
     postcss: {
