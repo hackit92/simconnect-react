f9f5069c4684c80d65f8c3e472654d1c796ed047
Abraham F. <39970401+hackit92@users.noreply.github.com>
2025-07-03 15:42:59 -0600

    Fix CartProvider context error

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./i18n";
+import { CurrencyProvider } from "./contexts/CurrencyContext";
+import { CartProvider } from "./contexts/CartContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
 import { Frame } from "./screens/Frame";
 import { LoginForm } from "./components/auth/LoginForm";
 import { SignupForm } from "./components/auth/SignupForm";
@@ -10,14 +13,20 @@ import { Success } from "./screens/Success";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignupForm />} />
-        <Route path="/checkout" element={<CheckoutForm />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/*" element={<Frame />} />
-      </Routes>
-    </BrowserRouter>
+    <CurrencyProvider>
+      <LanguageProvider>
+        <CartProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/signup" element={<SignupForm />} />
+              <Route path="/checkout" element={<CheckoutForm />} />
+              <Route path="/success" element={<Success />} />
+              <Route path="/*" element={<Frame />} />
+            </Routes>
+          </BrowserRouter>
+        </CartProvider>
+      </LanguageProvider>
+    </CurrencyProvider>
   </StrictMode>,
 );
--- src/screens/Frame/Frame.tsx
+++ src/screens/Frame/Frame.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { CurrencyProvider } from "../../contexts/CurrencyContext";
-import { CartProvider } from "../../contexts/CartContext";
-import { LanguageProvider } from "../../contexts/LanguageContext";
 import { useIsDesktop } from "../../hooks/useIsDesktop";
 import { MobileAppLayout } from "../../layouts/MobileAppLayout";
 import { DesktopAppLayout } from "../../layouts/DesktopAppLayout";
 
-const FrameContent = (): JSX.Element => {
+export const Frame = (): JSX.Element => {
   const isDesktop = useIsDesktop();
 
   if (isDesktop) {
@@ -21,17 +18,5 @@ const FrameContent = (): JSX.Element => {
         </div>
       </div>
     </div>
-  );
-};
-
-export const Frame = (): JSX.Element => {
-  return (
-    <CurrencyProvider>
-      <LanguageProvider>
-        <CartProvider>
-          <FrameContent />
-        </CartProvider>
-      </LanguageProvider>
-    </CurrencyProvider>
   );
 };
