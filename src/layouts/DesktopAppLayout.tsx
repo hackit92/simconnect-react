d18cb549c9162f73ed9839a6ead61b355cf018d7
Abraham F. <39970401+hackit92@users.noreply.github.com>
2025-07-02 19:39:42 -0600

    Create DesktopAppLayout component

---
+++ src/layouts/DesktopAppLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface DesktopAppLayoutProps {
+  children: React.ReactNode;
+}
+
+export const DesktopAppLayout: React.FC<DesktopAppLayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        {children}
+      </div>
+    </div>
+  );
+};
